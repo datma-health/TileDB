d29a0c275209c2392d176bfda7b1c626a3a00837
Nalini Ganapati <nganapati@gmail.com>
2018-10-03 13:05:39 -0700

    Add GCS to the travis matrix build

--- .travis/scripts/run_dfs_tests.sh
+++ .travis/scripts/run_dfs_tests.sh
@@ -2,15 +2,11 @@
 
 if [[ $INSTALL_TYPE != basic ]]; then
 	cd $TILEDB_BUILD_DIR && make examples && cd examples
-	STATUS = $?
-	if [[ $STATUS == 0 ]]; then
-		if [[ $INSTALL_TYPE == hdfs ]]; then
-			$TRAVIS_BUILD_DIR/examples/run_examples.sh "hdfs://localhost:9000/travis_test";		
-		elif [[ $INSTALL_TYPE == gcs && $TRAVIS_SECURE_ENV_VARS == true ]]; then
-			GOOGLE_APPLICATION_CREDENTIALS=$TRAVIS_BUILD_DIR/GCS.json $TRAVIS_BUILD_DIR/examples/run_examples.sh "gs://$GS_BUCKET/travis_test";
-		fi
-		diff travis_test.log $TRAVIS_BUILD_DIR/examples/expected_results;
-		STATUS = $?
+	if [[ $INSTALL_TYPE == hdfs ]]; then
+		$TRAVIS_BUILD_DIR/examples/run_examples.sh "hdfs://localhost:9000/travis_test";		
+	elif [[ $INSTALL_TYPE == gcs ]]; then
+		echo "GS_BUCKET=" $GS_BUCKET
+		GOOGLE_APPLICATION_CREDENTIALS=$TRAVIS_BUILD_DIR/GCS.json $TRAVIS_BUILD_DIR/examples/run_examples.sh "gs://$GS_BUCKET/travis_test";
 	fi
-	return STATUS
+	diff travis_test.log $TRAVIS_BUILD_DIR/examples/expected_results;
 fi
