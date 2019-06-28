736760be73398af3deeced4e29b483a5e9ade447
nalinigans <nalini@omicsautomation.com>
2019-06-28 11:28:51 -0700

    Time the travis DFS tests

--- .travis/scripts/run_dfs_tests.sh
+++ .travis/scripts/run_dfs_tests.sh
@@ -3,11 +3,11 @@
 if [[ $INSTALL_TYPE != basic ]]; then
 	cd $TILEDB_BUILD_DIR && make examples && cd examples
 	if [[ $INSTALL_TYPE == hdfs ]]; then
-		$TRAVIS_BUILD_DIR/examples/run_examples.sh "hdfs://localhost:9000/travis_test"
+		time $TRAVIS_BUILD_DIR/examples/run_examples.sh "hdfs://localhost:9000/travis_test"
 	elif [[ $INSTALL_TYPE == gcs ]]; then
-		GOOGLE_APPLICATION_CREDENTIALS=$TRAVIS_BUILD_DIR/.travis/resources/gcs/GCS.json $TRAVIS_BUILD_DIR/examples/run_examples.sh "gs://$GS_BUCKET/travis_test"
+		time GOOGLE_APPLICATION_CREDENTIALS=$TRAVIS_BUILD_DIR/.travis/resources/gcs/GCS.json $TRAVIS_BUILD_DIR/examples/run_examples.sh "gs://$GS_BUCKET/travis_test"
 	elif [[ $INSTALL_TYPE == azure ]]; then
-		$TRAVIS_BUILD_DIR/examples/run_examples.sh "wasbs://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_test"
+		time $TRAVIS_BUILD_DIR/examples/run_examples.sh "wasbs://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_test"
 	fi
 	if [[ ! $! -eq 0 ]]; then
         echo "Something wrong! run_examples.sh returned status code = $!"
