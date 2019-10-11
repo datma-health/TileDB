87c7fbe71bd75669ddac17c69f5892014f60169d
Clay Breshears <clay@omicsautomation.com>
2019-10-11 12:05:33 -0700

    adjust script for Travis osx run

--- .travis/scripts/install_openjpeg.sh
+++ .travis/scripts/install_openjpeg.sh
@@ -2,16 +2,15 @@
 set -ev
 
 # Install OpenJPEG2000 library with memory stream support 
-pushd ~
 git clone -b clay-dev https://github.com/OmicsDataAutomation/openjpeg.git
 
-cd openjpeg
+pushd openjpeg
 mkdir build ; cd build
 cmake ..
 make clean
 make 
 sudo make install
-   
+
 echo "OpenJPEG2000 library installed successfully"
 
 popd
