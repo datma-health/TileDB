4ddcd4bebbfaaec673ec561bbdb03c31194c5ffa
nalinigans <nalini@omicsautomation.com>
2019-10-16 19:48:25 -0400

    Add unit test for set_working_dir

--- test/src/c_api/test_tiledb_utils.cc
+++ test/src/c_api/test_tiledb_utils.cc
@@ -102,11 +102,12 @@ class TempDir {
 };
 
 TEST_CASE_METHOD(TempDir, "Test initialize_workspace", "[initialize_workspace]") {
-  return;
   std::string workspace_path = get_temp_dir()+"/"+workspace;
 
   TileDB_CTX *tiledb_ctx;
   CHECK(TileDBUtils::initialize_workspace(&tiledb_ctx, workspace_path, false) == 0); // OK
+  CHECK(!set_working_dir(tiledb_ctx, workspace_path));
+  CHECK(set_working_dir(tiledb_ctx, workspace_path+".non-existent"));
   CHECK(!tiledb_ctx_finalize(tiledb_ctx));
   CHECK(TileDBUtils::workspace_exists(workspace_path));
 
