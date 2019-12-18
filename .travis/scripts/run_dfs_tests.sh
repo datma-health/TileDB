0d6d9a4a12488e618b12811d3272fbf4abd5a239
nalinigans <nalini@omicsautomation.com>
2019-12-18 11:28:54 -0800

    Change travis azure check to match changed azure tst directory

--- .travis/scripts/run_dfs_tests.sh
+++ .travis/scripts/run_dfs_tests.sh
@@ -9,6 +9,7 @@ tiledb_utils_tests() {
 	$TILEDB_BUILD_DIR/test/test_tiledb_utils [move_across_filesystems] --test-dir $1
 }
 
+LOG= travis_test.log
 if [[ $INSTALL_TYPE != basic ]]; then
 	cd $TILEDB_BUILD_DIR && make examples && cd examples
 	if [[ $INSTALL_TYPE == hdfs ]]; then
@@ -20,12 +21,13 @@ if [[ $INSTALL_TYPE != basic ]]; then
 		time $TRAVIS_BUILD_DIR/examples/run_examples.sh "gs://$GS_BUCKET/travis_test"
 	elif [[ $INSTALL_TYPE == azure ]]; then
     tiledb_utils_tests "wasbs://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_unit_test" &&
-		time $TRAVIS_BUILD_DIR/examples/run_examples.sh "wasbs://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_test_azure"
+				time $TRAVIS_BUILD_DIR/examples/run_examples.sh "wasbs://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_test_azure"
+		 LOG=travis_test_azure.log
 	fi
 	if [[ ! $! -eq 0 ]]; then
         echo "Something wrong! run_examples.sh returned status code = $!"
         exit 1
     else
-		diff travis_test_azure.log $TRAVIS_BUILD_DIR/examples/expected_results
+		diff $LOG $TRAVIS_BUILD_DIR/examples/expected_results
 	fi
 fi
