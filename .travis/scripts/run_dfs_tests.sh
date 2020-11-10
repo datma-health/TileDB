20271a34574f66976963a226e794b985a5556da6
nalinigans <nalini@omicsautomation.com>
2020-11-10 14:19:55 -0800

    Add test_azure_blob_storage to list azure tests in travis

--- .travis/scripts/run_dfs_tests.sh
+++ .travis/scripts/run_dfs_tests.sh
@@ -27,7 +27,7 @@ if [[ $INSTALL_TYPE != basic ]]; then
     echo $AZURE_STORAGE_ACCOUNT
     echo "wasbs schema utils test" && tiledb_utils_tests "wasbs://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_unit_test" &&
     echo "az schema utils test" && tiledb_utils_tests "az://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_azure_blob_test" &&
-    echo "az schema storage test" && test_azure_blob_storage --test-dir "az://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_azure_storage_test" &&
+    echo "az schema storage test" && $TILEDB_BUILD_DIR/test/test_azure_blob_storage --test-dir "az://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_azure_storage_test" &&
     echo "az schema examples" && time $TRAVIS_BUILD_DIR/examples/run_examples.sh "az://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_test"
 #    echo "wasbs schema examples" && time $TRAVIS_BUILD_DIR/examples/run_examples.sh "wasbs://$AZURE_CONTAINER_NAME@$AZURE_ACCOUNT_NAME.blob.core.windows.net/travis_test"
   fi
