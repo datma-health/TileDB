c72c25c331a77d4615c3e588e3dcd4b372e938c1
Nalini Ganapati <nganapati@gmail.com>
2023-02-22 12:16:01 -0800

    Support for Ubuntu 22.04

--- cmake/Modules/FindS3Client.cmake
+++ cmake/Modules/FindS3Client.cmake
@@ -97,7 +97,8 @@ elseif(NOT AWSSDK_FOUND)
     PREFIX ${AWSSDK_PREFIX}
     URL ${AWSSDK_URL}
     PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/awssdk/build.patch &&
-                  patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/awssdk/cjson.patch
+                  patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/awssdk/cjson.patch &&
+		  patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/awssdk/eventstreamdecoder.patch
     CMAKE_ARGS
     -DBUILD_SHARED_LIBS=OFF
     -DENABLE_TESTING=OFF
