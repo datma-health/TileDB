2c456d45c0bc41de4d6a9c88a7fa5ab3a344ad29
Santhosh K M <santhosh.km@omicsautomation.com>
2023-03-01 16:04:35 +0530

    Renamed the file extension for .cc to .cpp for AWS

--- cmake/Modules/FindS3Client.cmake
+++ cmake/Modules/FindS3Client.cmake
@@ -94,8 +94,9 @@ elseif(NOT AWSSDK_FOUND)
                   ${CMAKE_CURRENT_SOURCE_DIR}/core/include/misc/dl_utils.h 
                   ${AWSSDK_PREFIX}/src/awssdk-build/aws-cpp-sdk-core/include/aws/core/utils/crypto/openssl &&
                   cp ${CMAKE_CURRENT_SOURCE_DIR}/core/src/misc/tdb_openssl_shim.cc
-                  ${CMAKE_CURRENT_SOURCE_DIR}/core/src/misc/dl_utils.cc 
-                  ${AWSSDK_PREFIX}/src/awssdk-build/aws-cpp-sdk-core/source/utils/crypto/openssl &&
+                  ${AWSSDK_PREFIX}/src/awssdk-build/aws-cpp-sdk-core/source/utils/crypto/openssl/tdb_openssl_shim.cpp &&
+                  cp ${CMAKE_CURRENT_SOURCE_DIR}/core/src/misc/dl_utils.cc 
+                  ${AWSSDK_PREFIX}/src/awssdk-build/aws-cpp-sdk-core/source/utils/crypto/openssl/dl_utils.cpp &&
                   patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/awssdk/build.patch &&
                   patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/awssdk/cjson.patch &&
 		  patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/awssdk/eventstreamdecoder.patch &&
