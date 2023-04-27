eaf4f6d3d57d043ceb4f7258e6428cedb4fd002a
eneskuluk <54481799+eneskuluk@users.noreply.github.com>
2023-04-27 16:54:23 -0700

    suppress deprecated declaration

--- cmake/Modules/FindS3Client.cmake
+++ cmake/Modules/FindS3Client.cmake
@@ -88,7 +88,7 @@ elseif(NOT AWSSDK_FOUND)
     DEPENDS aws-checksums-build)
 
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-maybe-uninitialized")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-maybe-uninitialized -Wno-deprecated-declaration")
   endif()
 
   ExternalProject_Add(awssdk-build
