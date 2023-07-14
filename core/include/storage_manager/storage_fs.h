90db680fe3176cba6d1502b0b113cc5290f6a7c1
Enes Kuluk <54481799+eneskuluk@users.noreply.github.com>
2023-07-14 12:44:09 -0700

    revert copyright change

--- core/include/storage_manager/storage_fs.h
+++ core/include/storage_manager/storage_fs.h
@@ -7,7 +7,6 @@
  *
  * @copyright Copyright (c) 2018-2019 Omics Data Automation Inc. and Intel Corporation
  * @copyright Copyright (c) 2020-2021 Omics Data Automation Inc.
- * @copyright Copyright (c) 2023 dātma, inc™
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
