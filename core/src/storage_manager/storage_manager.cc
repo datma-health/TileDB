7a0de0e24e69cd9bb6020b206c87f8d159cc2907
Nalini Ganapati <nalini@datma.com>
2023-07-26 22:30:08 -0700

    Error handling

--- core/src/storage_manager/storage_manager.cc
+++ core/src/storage_manager/storage_manager.cc
@@ -1695,7 +1695,7 @@ int StorageManager::create_group_file(const std::string& group) const {
   // Create file
   std::string filename = fs_->append_paths(group, TILEDB_GROUP_FILENAME);
   if(create_file(fs_, filename,  O_WRONLY | O_CREAT | O_SYNC, S_IRWXU) == TILEDB_FS_ERR) {
-    std::string errmsg = std::string("Failed to create group file");
+    std::string errmsg = std::string("Failed to create group file; ") + tiledb_ut_errmsg;
     PRINT_ERROR(errmsg);
     tiledb_sm_errmsg = TILEDB_SM_ERRMSG + errmsg;
     return TILEDB_SM_ERR;
@@ -1709,7 +1709,7 @@ int StorageManager::create_workspace_file(const std::string& workspace) const {
   // Create file
   std::string filename = fs_->append_paths(workspace, TILEDB_WORKSPACE_FILENAME);
   if(create_file(fs_, filename,  O_WRONLY | O_CREAT | O_SYNC, S_IRWXU) == TILEDB_UT_ERR) {
-    std::string errmsg = std::string("Failed to create workspace file; ");
+    std::string errmsg = std::string("Failed to create workspace file; ") + tiledb_ut_errmsg;
     PRINT_ERROR(errmsg);
     tiledb_sm_errmsg = TILEDB_SM_ERRMSG + errmsg;
     return TILEDB_SM_ERR;
@@ -1833,8 +1833,7 @@ int StorageManager::group_move(
   // Rename
   if(move_path(fs_, old_group_real, new_group_real)) {
     std::string errmsg = 
-        std::string("Cannot move group; ") + 
-        strerror(errno);
+        std::string("Cannot move group; ") + tiledb_ut_errmsg;
     PRINT_ERROR(errmsg);
     tiledb_sm_errmsg = TILEDB_SM_ERRMSG + errmsg;
     return TILEDB_SM_ERR;
