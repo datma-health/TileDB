dc43684e2e647c0b735ebb99fb19c86a961bb641
Gordon Chen <gordon@datma.com>
2023-08-24 13:35:44 -0700

    Copyright Updates

--- test/include/c_api/c_api_array_schema_spec.h
+++ test/include/c_api/c_api_array_schema_spec.h
@@ -7,6 +7,7 @@
  *
  * @copyright Copyright (c) 2016 MIT and Intel Corporation
  * @copyright Copyright (c) 2020 Omics Data Automation, Inc.
+ * @copyright Copyright (C) 2023 dātma, inc™
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
