9706a56b1196652420de31f8bb1fcb99abdff087
eneskuluk <54481799+eneskuluk@users.noreply.github.com>
2023-10-02 13:28:06 -0700

    fix

--- core/src/storage_manager/storage_manager.cc
+++ core/src/storage_manager/storage_manager.cc
@@ -2047,8 +2047,9 @@ void StorageManager::sort_fragment_names(
     std::string& fragment_name = fragment_names[i];
     PRINT_ERROR("fragment name is: "+fragment_name);
     std::string parent_fragment_name = parent_dir(fs_, fragment_name);
+    int skip = fragment_name[0] == '/' ? 2 : 1;
     std::string stripped_fragment_name = 
-        fragment_name.substr(parent_fragment_name.size() + 1);
+        fragment_name.substr(parent_fragment_name.size() + skip);
     PRINT_ERROR("parent fragment name is: "+parent_fragment_name);
     PRINT_ERROR("stripped fragment name is: "+stripped_fragment_name);
     assert(starts_with(stripped_fragment_name, "__"));
