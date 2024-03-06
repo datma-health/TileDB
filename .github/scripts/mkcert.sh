4d4956b6a4b4252f1186e002fb980d5d540f98ad
Melvin Lathara <melvin.lathara@gmail.com>
2024-03-05 16:05:18 -0800

    cert gen fix

--- .github/scripts/mkcert.sh
+++ .github/scripts/mkcert.sh
@@ -8,4 +8,4 @@ mv mkcert-v1.4.3-linux-amd64 /usr/local/bin/mkcert
 chmod +x /usr/local/bin/mkcert
 TRUST_STORES=system,java,nss mkcert -install
 
-CAROOT=. mkcert -cert-file cert.pem -key-file key.pem 0.0.0.0 localhost 127.0.0.1 azurite ::1
+mkcert -cert-file cert.pem -key-file key.pem 0.0.0.0 localhost 127.0.0.1 azurite ::1
